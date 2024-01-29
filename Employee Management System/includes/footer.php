cdd15b898742bde6fd17ea5c119ccf560eb87bb1
Imran Yousuf Wani <110186262+ImranSuraj@users.noreply.github.com>
2024-01-29 15:24:04 +0530

    Add files via upload

---
+++ Employee Management System/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By Imran Yousuf
+        </div>
+</footer>
